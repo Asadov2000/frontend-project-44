1cbf908729685951e7399ffa8539cce9f85ac0a8
Шахзод <112557790+Asadov2000@users.noreply.github.com>
2023-05-05 19:49:40 +0300

    Add files via upload

---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import calc from '../src/games/calc.js';
+import gameSession from '../src/index.js';
+
+gameSession(calc);
---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import prime from '../src/games/prime.js';
+import gameSession from '../src/index.js';
+
+gameSession(prime);
